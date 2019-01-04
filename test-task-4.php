50af9a33a6fce389c013089032c0494646c5e5c2
Roman Bolshakov <bolero2001@yandex.ru>
2019-01-04 14:13:47 +0200

    issue: inject instances in DataReceiver

--- test-task-4.php
+++ test-task-4.php
@@ -1,82 +1,16 @@
 <?php
-
-abstract class DataProvider {
-    abstract protected function get($input);
+function sum($a='', $b=''){
+    $a = str_split(strrev($a));
+    $b = str_split(strrev($b));
+    $adv = 0;
+    $result = array_map(function($item1, $item2) use (&$adv){
+        $digit = $item1 + $item2 + $adv;
+        if ($digit >= 10){
+            $adv = 1;
+            $digit -= 10;
+        } else { $adv = 0; } 
+        return $digit;
+    }, $a, $b);
+    if (1 === $adv) $result[] = $adv;
+    return implode('', array_reverse($result));
 }
-
-class RemoteDataProvider extends DataProvider{
-    private $host;
-    private $user;
-    private $password;
-
-    function __construct($host, $user, $password){
-        $this->host = $host;
-        $this->user = $user;
-        $this->password = $password;
-    }
-
-    public function get($input){
-        // ... getting data from remote data source
-    }
-}
-
-class CacheDataProvider extends DataProvider{
-    public function get($input){
-        // ... getting data from cache
-    }
-
-    public function isHitInput($input){
-        // check for cached data
-    }
-
-    public function cacheData($input, $data){
-        // cache data for input key (update if exists)
-    }
-}
-
-interface ILogger {
-    public function logException(Exception $ex);
-}
-
-class Logger implements ILogger{
-    public function logException(Exception $ex){
-        // log exception data
-    }
-}
-
-// decorator
-class DataReciever {
-    private $logger;
-    private $cacheDataProvider;
-    private $remoteDataProvider;
-
-    function __construct($host, $user, $password){
-        $this->logger = new Logger();
-        $this->cacheDataProvider = new CacheDataProvider();
-        $this->remoteDataProvider = new RemoteDataProvider($host, $user, $password);
-    }
-
-    public function get($input){
-        $data = [];
-        try{
-            if ($this->cacheDataProvider->isHitInput($input)){
-                $dataProvider = $this->cacheDataProvider;
-            }
-            else{
-                $dataProvider = $this->remoteDataProvider;
-            }
-            $data = $dataProvider->get($input);
-            $this->cacheDataProvider->cacheData($input, $data);
-            return $data;
-        }
-        catch(Exception $ex){
-            $this->logger->logException($ex);
-            // optional
-            throw $ex;
-        }
-    }
-}
-
-// usage (optional with try...catch block)
-$dataReciever = new DataReciever('https://api.example.com', 'test', 'password');
-$data = $dataReciever->get($input);
